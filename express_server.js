20c1a9535a99bb3bad80003d71c194a39e40300c
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-05-08 22:23:47 +0000

    adding delete functionality, working delete buttons added to /urls page

--- express_server.js
+++ express_server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const morgan = require('morgan');
+const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
-app.set('view engine', 'ejs');
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -14,18 +14,17 @@ const urlDatabase = {
 };
 
 let generatedShort = function generateRandomString() {
-  let result           = '';
-  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = "";
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let charactersLength = characters.length;
-  for (let i = 0; i < 6; i++ ) {
+  for (let i = 0; i < 6; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
 };
 
-
 //console.log(generateRandomString());
-
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -52,23 +51,27 @@ app.post("/urls", (req, res) => {
   const newShort = generatedShort();
   urlDatabase[newShort] = req.body.longURL;
   console.log(urlDatabase);
-  //console.log(generatedShort());  // Log the POST request body to the console
-  res.redirect("/urls/" + newShort);         // Respond with 'Ok' (we will replace this)
+  res.redirect("/urls/" + newShort); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  // let templateVars = {
-  //   shortURL: req.params.shortURL
-  //   longURL: urlDatabase['req.params.shortURL']
-  // }
-  console.log(req.params.shortURL)
+  //do i need to link this to anything? currently can only access when typed directly to browser?`
+  console.log(req.params.shortURL);
   res.redirect(urlDatabase[req.params.shortURL]);
 });
 
-app.get("/urls/:shortURL", (req,res) => {
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  console.log(shortURL);
+  delete urlDatabase[shortURL]; // delete from the DB
+
+  res.redirect("/urls");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL, //are you ever going to want to access the key? can you do this? or would you only need the value??
-    longURL: urlDatabase[req.params.shortURL],
+    longURL: urlDatabase[req.params.shortURL]
   };
   res.render("urls_show", templateVars);
 });
