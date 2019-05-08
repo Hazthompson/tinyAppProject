10bf5e399b6384cf2115e057f4ee44588b89a233
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-05-08 19:54:46 +0000

    adding random generate string function

--- express_server.js
+++ express_server.js
@@ -13,6 +13,20 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  let result           = '';
+   let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   let charactersLength = characters.length;
+   for (let i = 0; i < 6; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+
+
+//console.log(generateRandomString());
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
